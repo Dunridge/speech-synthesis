42f67dc2b87df60666829d9e5a8dbc513e97f88c
Dunridge <Polinka7max@gmail.com>
2024-02-17 15:16:34 -0800

    develop. add speach recognition

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 
-const buttonEl = document.querySelector('button');
-console.log(buttonEl);
+let recognition = new webkitSpeechRecognition();
 
-function sayHi() {
-    alert('hi');
+function onResult(event) {
+    const transcript = event.results[0][0].transcript;
+    const targetWord = transcript.split(' ')[0];
+    document.body.style.backgroundColor = targetWord;
 }
+
+function onClick() {
+    recognition.onresult = onResult;
+    recognition.start();
+}
+
+// function stopRecognition() {
+//     recognition.stop();
+// }
