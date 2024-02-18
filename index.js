7a15d95b19054e3ce8dc67ebd4bbf4f4e65955dc
Dunridge <Polinka7max@gmail.com>
2024-02-17 17:34:25 -0800

    develop. add div movement

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-
 let recognition = new webkitSpeechRecognition();
 
-function onResult(event) {
+function onControlBGResult(event) {
     const transcript = event.results[0][0].transcript;
-    const targetWord = transcript.split(' ')[0];
+    const targetWord = transcript.split(" ")[0];
     document.body.style.backgroundColor = targetWord;
 }
 
-function onClick() {
-    recognition.onresult = onResult;
+function onControlBGClick() {
+    recognition.onresult = onControlBGResult;
     recognition.start();
 }
 
-// function stopRecognition() {
-//     recognition.stop();
-// }
+// ---
+
+function onMoveDivResult(event) {
+    const transcript = event.results[0][0].transcript;
+    const targetWord = transcript.split(" ")[0];
+    const targetDiv = document.getElementsByClassName("target-div")[0];
+
+    
+    const moveDict = {
+        right: 10,
+        left: -10,
+        top: -10,
+        down: 10,
+    };
+    debugger;
+    if (targetWord === "right" || targetWord === "left") {
+        const currentLeft = parseInt(targetDiv.style.left) || 0;
+        const newLeft = currentLeft + moveDict[targetWord];
+        targetDiv.style.left = newLeft + "px";
+        debugger;
+    }
+
+    if (targetWord === "top" || targetWord === "down") {
+        const currentTop = parseInt(targetDiv.style.top) || 0;
+        const newTop = currentTop + moveDict[targetWord];
+        targetDiv.style.top = newTop + "px";
+        debugger;
+    }
+}
+
+function onMoveDivClick() {
+    recognition.onresult = onMoveDivResult;
+    recognition.start();
+}
